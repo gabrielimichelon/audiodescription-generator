bb8558bb6e06b9a7719750bf11e82da087b5f994
gabrielimichelon <gabrieli.michelon@gmail.com>
2023-06-28 22:45:45 -0300

    change shot detect

--- controller.py
+++ controller.py
@@ -1,8 +1,11 @@
 import cv2
 import os
 import time
+import json
 from scenedetect import detect, ContentDetector
+from darkflow.net.build import TFNet
 from parser import Parser
+
 
 class ADGenerator:
     def __init__(self):
@@ -13,32 +16,60 @@ class ADGenerator:
 
     def video_analysis(self):
         frames = self.analyze_shot()
-        self.split_frames(frames, self.video_input, self.results_out+"/frames/")
+        self.split_frames(frames, self.video_input)
+        # print(self.final_result)
 
+    def write_shots(self, list_shot):
+        path = self._path_exists("\shots")
+        f = open(path+"\change_shots_detected.json", "w")
+        f.write(',\n'.join(list_shot))
+        f.close()
+
+    def predict_midia():
+
+
+        options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1}
+
+        tfnet = TFNet(options)
+
+        imgcv = cv2.imread("./sample_img/sample_dog.jpg")
+        result = tfnet.return_predict(imgcv)
+        print(result)
 
     def analyze_shot(self):
         scene_list = detect(self.video_input, ContentDetector())
-        print(scene_list)
+        list_frames = []
+        shot_frames = []
         for i, scene in enumerate(scene_list):
-            print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-            i+1,
-            scene[0].get_timecode(), scene[0].get_frames(),
-            scene[1].get_timecode(), scene[1].get_frames(),))
-
-    def distinct_img(self):
-        pass
-    
-    def write_frame(self):
-        pass
-
-    def split_frames(self, frames_list, video_path, frames_path):
-        cap = cv2.VideoCapture(f"{video_path}")
+            value = f'"Scene": {i+1}, "Start": ["{scene[0].get_timecode()}", "{scene[0].get_frames()}"], "End": ["{scene[1].get_timecode()}", "{scene[1].get_frames()}"]'
+            shot_frames.append(value) #ver se vai ser utilizado esse dado
+            list_frames.append(scene[0].get_frames())
         
-        if not os.path.exists(frames_path):
-            os.makedirs(frames_path)
+        self.write_shots(shot_frames)
+       
+        return list_frames
+    
+    def write_frame(self, cap, frame, directory):
+        try:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
+            ret, _frame = cap.read()
+            path = directory+f"/frame_{frame}.jpg"
+            cv2.imwrite(path, _frame)
+            if f"frame_{frame}.jpg" not in self.final_result:
+                self.final_result[f"frame_{frame}.jpg"] = {"labels": [], "text_from_labels": "", "text_from_image": "", "audio_url": ""}
+        except:
+            pass # for null frames
+    
+    def _path_exists(self, directory):
+        if not os.path.exists(self.results_out_path+directory):
+            os.makedirs(self.results_out_path+directory)
+        return self.results_out_path+directory
+
+    def split_frames(self, frame_list, video_path):
+        cap = cv2.VideoCapture(video_path)
 
         for frame in frame_list:
-            self.write_frame(cap, frame, directory)
+            self.write_frame(cap, frame, self._path_exists("/train"))
 
         cap.release()
         cv2.destroyAllWindows() # destroy all opened windows
